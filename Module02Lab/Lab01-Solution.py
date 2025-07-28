a16fff251ff5dcafae4168d83457eb340057c06c
Richard Howells <richard@dynamisys.co.uk>
2025-07-28 13:38:26 +0100

    Fixed calculation bug, added documentation nots to the print calls

--- Module02Lab/Lab01-Solution.py
+++ Module02Lab/Lab01-Solution.py
@@ -7,6 +7,9 @@ print(type(j), id(j))
 k = True
 print(type(k), id(k))
 
+l = i
+print(type(l), id(l))
+
 rate_per_widget = 0.50
 quantity_made = 30
 
@@ -30,16 +33,36 @@ pay = quantity_made * rate_per_widget
 
 print(pay)
 
-### With a bonus over 35 units
+### With higher pay over 35 units - expected result 28.75
 
 quantity_made = 50
-pay = quantity_made * rate_per_widget
 
 if quantity_made > 35:
     excess_units = quantity_made - 35
-    excess_pay = quantity_made * rate_per_widget * 0.5
+    excess_pay = excess_units * rate_per_widget * 0.5
+else:
+    excess_pay = 0
 
 basic_pay = quantity_made * rate_per_widget
 pay = basic_pay + excess_pay
+    
+print("Higher pay over 35 units", pay)
+
+### Over 45 units - With a 10% bonus on entire pay - expected 31.625
+# note that the actual shows a rounding error 
+
+quantity_made = 50
+
+if quantity_made > 35:
+    excess_units = quantity_made - 35
+    excess_pay = excess_units * rate_per_widget * 0.5
+else:
+    excess_pay = 0
+
+basic_pay = quantity_made * rate_per_widget
+pay = basic_pay + excess_pay
+
+if quantity_made > 45:
+    pay = pay * 1.1
     
 print(pay)
