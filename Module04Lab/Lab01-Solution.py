64ecf710547ea4bb62088cad54bc06a9584de574
Richard Howells <richard@dynamisys.co.uk>
2025-07-28 14:57:14 +0100

    Implement proper logic for pay calculation.  Add documenting print() calls

--- Module04Lab/Lab01-Solution.py
+++ Module04Lab/Lab01-Solution.py
@@ -1,11 +1,25 @@
 
-def basic_pay(rate, items):
-    return rate * items
+def basic_pay(rate_per_widget, quantity_made, enhancement_threshold = 35):
+    if quantity_made > enhancement_threshold:
+        excess_units = quantity_made - enhancement_threshold
+        excess_pay = excess_units * rate_per_widget * 0.5
+    else:
+        excess_pay = 0
+
+    basic_pay = quantity_made * rate_per_widget
+    pay = basic_pay + excess_pay
+
+    if quantity_made > 45:
+        pay = pay * 1.1
+
+    return pay
 
 
+print("Calling from for loop")
 for items in [30, 40, 50]:
     print(basic_pay(0.5, items)) 
 
+print("Calling from list comprehension")
 print([basic_pay(0.5, items) for items in [30, 40, 50]])   
 
 def factorial(n):
